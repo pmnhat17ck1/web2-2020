ce2a084bf4a5ed0fcf81a5e2a95c409667ac23cf
pmnhat17ck1 <63638162+pmnhat17ck1@users.noreply.github.com>
2020-08-05 22:12:41 +0700

    fix
    client.js

--- client.js
+++ client.js
@@ -7,7 +7,7 @@ const { router } = require('./routes/router');
 const app = express();
 const port = process.env.PORT || 3002;
 
-app.set('views', './views');
+app.set('views', path.join(__dirname,'/views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,7 +18,9 @@ app.use(cookieSession({
 }));
 
 app.use(express.static(path.join(__dirname, '/')));
-
+app.use(express.static(path.join(__dirname, '/css')));
+app.use(express.static(path.join(__dirname, '/vendor')));
+app.use(express.static(path.join(__dirname, '/js')));
 app.use(require('./middleware/auth'));
 
 app.listen(port, function() {
