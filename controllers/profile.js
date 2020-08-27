0b890832e5f55f1e8f1447e0aa5c869c861b6b6a
NguyenBao23031999 <58360282+NguyenBao23031999@users.noreply.github.com>
2020-08-27 10:06:24 +0700

    profile
    upload...

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,11 +1,29 @@
-// module.exports.getprofile = function getprofile(req, res) {
-//     if (req.currentUser)
-//         res.render('profile');
-//     else res.redirect('/');
-// };
+const asyncHandler = require('express-async-handler');
+const { request } = require('./request');
+
+module.exports.getprofile = asyncHandler(async function getprofile(req, res) {
+    if (req.currentUser) {
+
+        const Acount = await profile(req.currentUser.id);
+        console.log(Acount);
+        return res.render('profile', { errors: null, acount: Acount });
+    }
+    res.redirect('/');
+});
 
 
-// module.exports.postfile = function postfile(req, res, next) {
-//     console.log(req.file);
-//     res.render('profile');
-// };
+module.exports.postprofile = asyncHandler(async function postprofile(req, res, next) {
+    const password1 = req.body.password1;
+    const password2 = req.body.password2;
+    const Acount = await profile(req.currentUser.id);
+
+    // console.log(Paper);
+    const result = await request('POST', '/change-password', { password1: password1, password2: password2, id: req.currentUser.id });
+    console.log(result.result);
+    res.render('profile', { errors: result.result, acount: Acount });
+});
+
+const profile = asyncHandler(async function(id) {
+    const Acount = await request('POST', '/profile', { userId: id });
+    return Acount.profile.length > 0 ? Acount.profile : [];
+});
