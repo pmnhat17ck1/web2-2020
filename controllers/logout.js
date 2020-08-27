829f9c19f83f45584cba1baa152ca8d71598ccfa
NguyenBao23031999 <58360282+NguyenBao23031999@users.noreply.github.com>
2020-08-27 10:07:33 +0700

    logout
    adđ

--- controllers/logout.js
+++ controllers/logout.js
@@ -2,3 +2,8 @@ module.exports.getlogout = function getlogout(req, res, next) {
     delete req.session.userId;
     res.redirect('/');
 };
+
+module.exports.getadminlogout = function(req, res, next) {
+    delete req.session.userIdAdmin;
+    res.redirect('/admin/login');
+};
