1c149845df1596766c55d7f3d72bad0b8348a044
NguyenBao23031999 <58360282+NguyenBao23031999@users.noreply.github.com>
2020-08-27 10:09:28 +0700

    login
    fix

--- controllers/login.js
+++ controllers/login.js
@@ -2,7 +2,7 @@ const asyncHandler = require('express-async-handler');
 const { request } = require('./request');
 const url = require('url');
 module.exports.getlogin = function getlogin(req, res, next) {
-    res.render('login');
+    res.render('login', { errors: null });
 };
 
 module.exports.postlogin = asyncHandler(async function postlogin(req, res, next) {
@@ -10,17 +10,9 @@ module.exports.postlogin = asyncHandler(async function postlogin(req, res, next)
     const UserId = await request('POST', '/login', req.body);
     const id = UserId.id;
     console.log(id);
-    if (id <= 0) {
-        req.session.views = (req.session.views || 0) + 1;
-        console.log("count " + req.session.views);
-        const url_login = url.parse(req.url);
-        if (req.session.views > 3) {
-            return res.redirect('/login');
-        } else {
-            return res.redirect('/');
-        }
+    if (id == 0) {
+        return res.render('login', { errors: UserId.result });
     }
     req.session.userId = id;
-    delete req.session.views;
-    res.redirect('/');
+    res.redirect('/home/accountmanagement/profile');
 });
